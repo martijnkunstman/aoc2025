efa53dba3c7719b9d9f8ad1fad17935ddf57359b
Martijn Kunstman <martijn@kunstman.nl>
2025-12-08 20:27:28 +0100

    day 8

---
+++ day8/main.js
@@ -0,0 +1,144 @@
+
+let inputData = [];
+let objectData = [];
+let result1 = [];
+let result2 = 0;
+
+fetch(0 ? "sample.txt" : "input.txt")
+    .then(response => response.text())
+    .then(data => {
+        //-------------------------- parse data --------------------------
+
+        inputData = data.split("\r\n");
+
+        for (let i = 0; i < inputData.length; i++) {
+            //parse into array of numbers
+            inputData[i] = inputData[i].split(",").map(Number);
+            objectData[i] = { id: i, x: inputData[i][0], y: inputData[i][1], z: inputData[i][2], distances: [] };
+        }
+
+        //(x1-y1)^2 + (x2-y2)^2 + (x3-y3)^2
+
+        for (let i = 0; i < inputData.length; i++) {
+            for (let j = 0; j < inputData.length; j++) {
+                if (i !== j) {
+                    let dist = Math.pow(inputData[i][0] - inputData[j][0], 2) + Math.pow(inputData[i][1] - inputData[j][1], 2) + Math.pow(inputData[i][2] - inputData[j][2], 2);
+                    objectData[i].distances.push({ id: j, dist: dist, parent: i });
+                }
+                else {
+                    objectData[i].distances.push({ id: j, dist: 0, parent: i });
+
+                }
+            }
+        }
+
+        
+
+        let distances = [];
+        for (let i = 0; i < objectData.length; i++) {
+            for (let j = 0; j < objectData[i].distances.length; j++) {
+                if (objectData[i].distances[j].dist !== 0) {
+                    distances.push(objectData[i].distances[j]);
+                }
+            }
+        }
+
+        
+
+
+        distances.sort((a, b) => a.dist - b.dist);
+        //check if there are duplicates in distances
+        removeEveryOdd = (arr) => arr.filter((_, index) => index % 2 === 0);
+        distances = removeEveryOdd(distances);
+        //get only first 1000 distances
+        let distancesCOpy = JSON.parse(JSON.stringify(distances));
+        console.log("points:");
+        console.log(distancesCOpy[5436]);
+        
+        //840, 451 -> 5437
+        //670, 316 -> 5436
+        //743, 566 -> 5435
+        //305, 61 -> 5434
+
+        //670,316
+        console.log(objectData[670].x * objectData[316].x);//8289474414 to high
+        //743, 566
+        console.log(objectData[743].x * objectData[566].x);//42047840 (wrong input 288509904 to high -> 42047840)
+
+
+       
+
+        
+
+        //876 connects?
+
+        //583, 415, 867
+
+        distances = distances.slice(0, 5436);///!!!!!!!!!!!!!!!!!!!!!! 5436 is de connection...
+        //find connected parent id groups
+        let connectedGroups = []
+        //create groups
+        for (let i = 0; i < distances.length; i++) {
+            let found = false;
+            for (let j = 0; j < connectedGroups.length; j++) {
+                if (connectedGroups[j].includes(distances[i].id)) {
+                    found = true;
+                }
+                if (connectedGroups[j].includes(distances[i].parent)) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                connectedGroups.push([distances[i].id, distances[i].parent]);
+                //remove from distances
+                distances.splice(i, 1);
+            }
+            for (let ii = 0; ii < distances.length; ii++) {
+                for (let j = 0; j < connectedGroups.length; j++) {
+                    if (connectedGroups[j].includes(distances[ii].id) || connectedGroups[j].includes(distances[ii].parent)) {
+                        connectedGroups[j].push(distances[ii].parent);
+                        connectedGroups[j].push(distances[ii].id);
+                        connectedGroups[j] = [...new Set(connectedGroups[j])];
+                        distances.splice(ii, 1);
+                        break;
+                        //ii--;
+                        //i--;
+                    }
+                }
+            }
+        }
+
+        //remove duplicates in groups
+        for (let i = 0; i < connectedGroups.length; i++) {
+            connectedGroups[i] = [...new Set(connectedGroups[i])];
+        }
+
+        //make array with lengths of groups
+        let groupLengths = connectedGroups.map(group => group.length);
+        //order by length descending
+        groupLengths.sort((a, b) => b - a);
+
+
+
+        console.log(connectedGroups);
+        console.log("groupLengths:");
+        console.log(groupLengths);
+
+
+
+        console.log(distances);
+        console.log("Result 1: " + (groupLengths[0] * groupLengths[1] * groupLengths[2]));
+
+        //219539 -> too high
+
+        console.log(objectData);
+        //-------------------------- part 2 --------------------------
+        // code here....
+
+
+        console.log("Result 2: " + result2);
+        //--------------------------- end ----------------------------
+    })
+    .catch(error => {
+        console.error('Error fetching the file:', error);
+    });

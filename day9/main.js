de4640523c7014d32be5e6efa6fd5ed0a0836d59
Martijn Kunstman <martijn@kunstman.nl>
2025-12-09 11:10:16 +0100

    day 9 part 2 notes and comments in solution

--- day9/main.js
+++ day9/main.js
@@ -19,26 +19,32 @@ fetch(0 ? "sample.txt" : "input.txt")
 
         for (let i = 0; i < inputData.length; i++) {
             for (let j = i + 1; j < inputData.length; j++) {
+                //i forggot to add the +1 in the first run
                 let width = Math.abs(inputData[i][0] - inputData[j][0]) + 1;
                 let height = Math.abs(inputData[i][1] - inputData[j][1]) + 1;
                 let surface = width * height;
                 part1Numners.push(surface);
             }
         }
-
+        //nice and easy find the highest number with the Math.max...
         let getHighest = Math.max(...part1Numners);
-        result1 = getHighest;
+        result1 = getHighest;//why did i do this? and not straight away result1 = Math.max(...part1Numners);
 
-        //console.log(inputData);
+        //console.log(inputData); //to see if my data is parsed right
 
-        console.log("Result 1: " + result1);
+        console.log("Result 1: " + result1);//
 
         //4746100212 to low....
         //4746238001 is right....
 
         //-------------------------- part 2 -------------------------- 
 
-        //visualise the line between points on a canvas
+        //visualise the line between points on a canvas to get a sense of what we are working with...
+        //analyse the dimmensions of the grid by eye studing the inputdata...
+        //we seem to have max 100000 x 100000 grid.... 
+
+        //that is not going to work on an uint8array grid... I(i tried...)
+        //let grid = new Uint8Array(100000 * 100000); //no way
 
         let canvas = document.createElement('canvas');
         canvas.width = 1000;
@@ -59,8 +65,11 @@ fetch(0 ? "sample.txt" : "input.txt")
         ctx.closePath();
         ctx.stroke();
 
-        //there is only straight lines between points
-        //check all squares i can make and skip those that cross a line
+        //is see now.... there is only straight lines between points
+        //i also realise there is only the bggest squares possible if there is not a point inside AND it contains one of the weird point..
+        //BUT than there is only a combination possible where there is a sqquare ABOVE or UNDER the two special points....
+       
+        //start taking it down step by step.......
 
         //make an array of all the squares and order them big to low
         let squares = [];
@@ -72,8 +81,11 @@ fetch(0 ? "sample.txt" : "input.txt")
                 squares.push({ x1: inputData[i][0], y1: inputData[i][1], x2: inputData[j][0], y2: inputData[j][1], surface: surface });
             }
         }
-        //now check for all the squares if there is a point in them from the top down
+        //nice and easy sort it....
         squares.sort((a, b) => b.surface - a.surface);
+     
+        //now check for all the squares if there is a point in them from the top down
+        // (i thought first this would give a solution....)
         for (let s = 0; s < squares.length; s++) {
             let hasPointInside = false;
             squares[s].pointInside = false;
@@ -88,11 +100,19 @@ fetch(0 ? "sample.txt" : "input.txt")
                 }
             }
         }
+        //to check if the squares were made right...
         console.log(squares);
+
         //
-        //find first square without a point inside
+        //find first square without a point inside (based on previous check)
+        //
+        // (that is not enoough... or not even neeeded, did not check that...)
+        //
+        //NOW the magic....
+        //
         //and that has point(94539,50089) or point(94539,48701)
-        //but not cross the line!!!!!
+        //
+        //but not cross the line!!!!! (so points a at the right side top or down from the special point... i had this wronmg firsttime...)
         //
         for (let s = 0; s < squares.length; s++) {
             if (!squares[s].pointInside) {
